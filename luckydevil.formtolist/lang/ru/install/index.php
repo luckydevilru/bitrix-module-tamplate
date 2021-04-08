87677200cf6f717dee5270584385f2cc1df7b207
Sergo <prokh.sergey@gmail.com>
2021-04-08 14:32:06 +0300

    First

---
+++ luckydevil.formtolist/lang/ru/install/index.php
@@ -0,0 +1,6 @@
+<?
+$MESS["luckydevil.formtolist_MODULE_NAME"] = "Form to List";
+$MESS["luckydevil.formtolist_MODULE_DESC"] = "Заполняем универсальные списки из форм битрикс24";
+$MESS["luckydevil.formtolist_PARTNER_NAME"] = "LuckyDevil";
+$MESS["luckydevil.formtolist_PARTNER_URI"] = "https://lucky-devil.ru";
+?>
